2488b6e4309d9443b9a909df3fd9b87e7af25f3e
clearedd <mrcreaper101@gmail.com>
2023-12-02 03:22:14 +0200

    updated title filtering

--- filter.js
+++ filter.js
@@ -9,10 +9,11 @@ program
     .option(`--channel <name>`, `Filter by channel name`)
     .option(`--date <date>`, `Filter by date, examples: >2011-07-04 (before)`)
     .option(`--category <category>`, `Filter by category (check catLb.txt)`)
-    .option(`--title <words>`, `title must include. seperate with ","`)
+    .option(`--title <words>`, `title must include, prefix with "-" to exclude. seperate with ","`)
     .option(`--noheader <headers>`, `list headers to not include. seperate with ","`)
     .option(`--overview`, `gives an overview of (probably) a channel`)
     .option(`--overviewlimit <limit>`, `list limit for overview`, 100)
+    .option(`-d --debug`)
 
 program.parse(process.argv);
 const options = program.opts();
@@ -93,23 +94,26 @@ var formatNum = x => Intl.NumberFormat(`en`, { notaion: `compact` }).format(x);
         tags: {},
         words: {},
     };
+    var videos = {};
     fs.createReadStream(csvFile)
         .pipe(csv())
         .on('data', async x => {
             checked++;
-            readline.cursorTo(process.stdout, 0);
-            readline.clearLine(process.stdout, 0);
-            process.stdout.write(`${(checked / totalRows * 100).toFixed(2)}%`);
+            if (!options.debug) {
+                readline.cursorTo(process.stdout, 0);
+                readline.clearLine(process.stdout, 0);
+                process.stdout.write(`${(checked / totalRows * 100).toFixed(2)}%`);
+            }
             // total
 
-            String(x.tags).toLowerCase().split(`,`).forEach(tag => {
+            String(x.tags).toLocaleLowerCase().split(`,`).forEach(tag => {
                 if (tag)
                     if (!total.tags[tag])
                         total.tags[tag] = 1;
                     else total.tags[tag]++;
             });
 
-            String(x.title).toLowerCase()
+            String(x.title).toLocaleLowerCase()
                 .replace(/[^A-Za-z\s]/g, '') // remove non characters
                 .split(` `)
                 .forEach(word => {
@@ -125,35 +129,39 @@ var formatNum = x => Intl.NumberFormat(`en`, { notaion: `compact` }).format(x);
                 if (options.views.startsWith(`<`) && x.views < options.views.replace(`<`, ``)) return; // more then
                 if (options.views.startsWith(`=`) && x.views != options.views.replace(`=`, ``)) return; // more then
             }
-            if (options.channel && options.channel.toLowerCase() != x.author.toLowerCase()) return;
+            if (options.channel && options.channel.toLocaleLowerCase() != x.author.toLocaleLowerCase()) return;
             if (options.date) {
                 if (options.date.startsWith(`>`) && new Date(x.upload) > new Date(options.views.replace(`>`, ``))) return; // less then
                 if (options.date.startsWith(`<`) && new Date(x.upload) < new Date(options.views.replace(`<`, ``))) return; // more then
                 if (options.date.startsWith(`=`) && new Date(x.upload) != new Date(options.views.replace(`=`, ``))) return; // more then
             }
             if (options.category && options.category != x.category) return;
-            if (options.title && !String(options.title).toLowerCase().split(`,`).some(y => String(x.title).toLowerCase().includes(y))) return;
+            if (options.title && !String(options.title).toLocaleLowerCase().split(`,`).every(y => 
+                y.startsWith(`-`) ? !String(x.title).toLocaleLowerCase().includes(y.replace(`-`, ``)) : String(x.title).toLocaleLowerCase().includes(y)
+            )) return;
 
             // overview
             if (options.overview) {
                 overview.views += Number(x.views);
                 overview.categories[x.category] += 1;
                 overview.channels[x.author] += 1;
-                String(x.tags).toLowerCase().split(`,`).forEach(tag => {
+                String(x.tags).toLocaleLowerCase().split(`,`).forEach(tag => {
                     if (tag)
                         if (!overview.tags[tag])
                             overview.tags[tag] = 1;
                         else overview.tags[tag]++;
                 });
-                String(x.title).toLowerCase()
+                String(x.title).toLocaleLowerCase()
                     .replace(/[^A-Za-z\s]/g, '') // remove non characters
-                    .split(/[ ]/)
+                    .split(` `)
                     .forEach(y => {
                         if (y)
                             if (!overview.words[y])
                                 overview.words[y] = 1;
                             else overview.words[y]++;
                     });
+
+                videos[x.videoid] = x.title;
             }
 
             // save
@@ -161,8 +169,11 @@ var formatNum = x => Intl.NumberFormat(`en`, { notaion: `compact` }).format(x);
             csvWriter.writeRecords([x])
                 .catch((err) => console.error(`Error writing CSV file: ${err}`));
         }).on(`close`, () => {
-            readline.cursorTo(process.stdout, 0);
-            readline.clearLine(process.stdout, 0);
+            if (!options.debug) {
+                readline.cursorTo(process.stdout, 0);
+                readline.clearLine(process.stdout, 0);
+            }
+            if(!found) return console.log(`No results :(`);
             console.log(`Done! ${found}/${totalRows} (${(found / totalRows * 100).toFixed(2)}%)`);
             // overview
             if (options.overview) {
@@ -175,6 +186,9 @@ var formatNum = x => Intl.NumberFormat(`en`, { notaion: `compact` }).format(x);
                 Object.keys(options)
                     .filter(x => ![`overview`, `overviewlimit`].includes(x))
                     .forEach(x => ov += `${x}: ${options[x]}<br>\n`);
+
+                    if(!Object.keys(options))
+                    ov += `**NONE?!**\n`
 
                 // ## Dataset
                 ov += `\n## Dataset\n`;
@@ -195,6 +209,7 @@ var formatNum = x => Intl.NumberFormat(`en`, { notaion: `compact` }).format(x);
                 ov += `<br>\nwords sum: **${formatNum(wordsSum)}**\n\n` +
                     `num | count | tag | % of total\n---|---|---|---\n`;
                 fs.appendFileSync(overviewFile, ov);
+                ov = ``;
 
                 KVtoArr(overview.words)
                     .sort((a, b) => a[1] - b[1]).reverse()
@@ -205,13 +220,13 @@ var formatNum = x => Intl.NumberFormat(`en`, { notaion: `compact` }).format(x);
                     })
 
                 // ## Tags
-                ov = ``;
-                ov += `\n\n## Tags\n`;
+                ov += `\n## Tags\n`;
                 ov += `\ndiffrent tags: **${formatNum(Object.keys(overview.tags).length)}**`;
                 let tagsSum = Object.values(overview.tags).reduce((a, b) => a + b, 0);
                 ov += `<br>\ntags sum: **${formatNum(tagsSum)}**\n\n` +
                     `num | count | tag | % of total\n---|---|---|---\n`;
                 fs.appendFileSync(overviewFile, ov);
+                ov = ``;
 
                 KVtoArr(overview.tags)
                     .sort((a, b) => a[1] - b[1]).reverse()
@@ -220,6 +235,16 @@ var formatNum = x => Intl.NumberFormat(`en`, { notaion: `compact` }).format(x);
                         fs.appendFileSync(overviewFile, `${i + 1} | ${String(x[1])} | ${x[0]}` +
                             `${x[1] / total.tags[x[0]] == 1 ? `` : ` | ${(x[1] / total.tags[x[0]] * 100).toPrecision(3)}%`}\n`);
                     })
+
+                // ## Videos
+                ov += `\n## Videos\n\n`;
+                fs.appendFileSync(overviewFile, ov);
+                KVtoArr(videos)
+                    .sort((a, b) => a[1] < b[1]).reverse()
+                    .splice(0, options.overviewlimit)
+                    .forEach(x =>
+                        fs.appendFileSync(overviewFile, `[${x[1]}](https://www.youtube.com/watch?v=${x[0]})<br>\n`)
+                    );
             }
         });
 })();
